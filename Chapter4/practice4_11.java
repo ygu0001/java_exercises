25a0b284feb448fd95bf78c6622a4c719d5e1d46
Clair Yiting Gu <clair.yiting.gu@gmail.com>
2018-03-12 13:07:37 -0400

    4.11
    (Find the Unicode of a character) Write a program that receives a character and
    displays its Unicode.

---
+++ Chapter4/practice4_11.java
@@ -0,0 +1,21 @@
+package Chapter4;
+
+import java.util.Scanner;
+
+/**
+ * (Decimal to hex) Write a program that prompts the user to enter an integer between 0 and 15 and displays its corresponding hex number. 
+ * Here are some sample runs:
+ * @author guyiting
+ *
+ */
+
+public class practice4_11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a decimal value (0 to 15):");
+
+	}
+
+}
