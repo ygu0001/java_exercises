fdde2c7d1e22e6bd75b25422b92983e7690dd0b9
Clair Yiting Gu <clair.yiting.gu@gmail.com>
2018-03-31 15:15:31 -0400

    5_18
    5_18

---
+++ Chapter5/practice5_18.java
@@ -0,0 +1,44 @@
+package Chapter5;
+/**
+*
+*@author Clair Yiting Gu
+*github:https://github.com/ygu0001/java_exercises/tree/master/Chapter5
+*
+*5.18
+* 1
+* 1 2
+*  1 2 3
+* 1 2 3 4
+* 1 2 3 4 5
+* 1 2 3 4 5 6
+*
+*/
+
+public class practice5_18 {
+
+	public static void main(String[] args) {
+		
+	
+
+		 
+		        // write your code here
+
+		        int size = 2;
+		        for (int i = 1; i <=size ; i++) {
+		        for (int j = 1; j <= i ; j++) {
+		            System.out.print(j+" ");
+		        }
+		            System.out.println();
+		            }
+
+		        }
+
+
+
+
+
+
+
+	}
+
+}
