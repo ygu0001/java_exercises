da8318fe22dc95a394c571c9ac37cc9c7298479a
Clair Yiting Gu <clair.yiting.gu@gmail.com>
2018-04-16 17:18:52 -0400

    Chapter9

--- Chapter5/practice5_18.java
+++ Chapter5/practice5_18.java
@@ -41,4 +41,4 @@ public class practice5_18 {
 
 	}
 
-}
+
---
+++ Chapter9/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Circle circle = new Circle();
+        circle.setRadius(4);
+        System.out.println(circle.getArea());
+    }
+}
+
+
+class Circle {
+    double radius = 1;
+    Circle(){
+
+    }
+
+    Circle(double newRadius){
+        this.radius = newRadius;
+    }
+
+    double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+    double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+
+    void setRadius(double newRadius){
+        this.radius = newRadius;
+    }
+}
